f1c2c2eeec92345ffdf59b3ae05770d31b3a00cb
Nate Jacobs <natesjacobs@gmail.com>
2023-06-10 10:36:51 -0700

    calendar view text centered

--- README.txt
+++ README.txt
@@ -4,6 +4,8 @@ SVG processing
 - export from figma / illustrator
 - add <script> element to increment UNIX timestamp every second
 - replace all " with /" so that it can be used in solidity return
-- replace each value in the calendar (e.g. year, day, etc) with a unique string to be replaced by solidity function
+- replace each value in the calendar (e.g. year, day, etc) with a unique string (e.g. DD, YY) to be replaced by solidity function
 - remove all line breaks so that all elements are on a single line
 - copy paste into .sol contract function
+
+
--- earth-calendar.sol
+++ earth-calendar.sol
@@ -1,82 +1,77 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: Unlicense
 
+pragma solidity >=0.8.2 <0.9.0;
 
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-// Test return of SVG
+contract earthCalendar {
 
-function calendarView() public view pure returns (string) {
+    function getDateTime() public view returns(uint[6] memory) {
+        // init
+        uint t = block.timestamp;
+        uint secondsPerQuarter = 7884000; // 91.25 days in a quarter (accounts for leap year)
+        uint secondsPerDay =  86400;
+        uint secondsPerHour = 3600;
+        // year
+        uint y = uint256(block.timestamp / 31536000); // number of seconds in a non-leap year
+        // quarter and day of quarter
+        uint secondsCurrentQuarter = t % secondsPerQuarter; 
+        uint q = ((t / secondsPerQuarter) % 4) + 1;
+        uint d1 = secondsCurrentQuarter / secondsPerDay + 1;
+        // day of week
+        uint d2 = (block.timestamp / secondsPerDay + 4) % 7;
+        // hour
+        uint h = (block.timestamp / secondsPerHour) % 24;
 
-  return "<svg width="429" height="162" viewBox="0 0 429 162" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <circle cx="83.5" cy="73.5" r="52" fill="#1D1D1D" stroke="#414141"/>
-  <circle cx="257.5" cy="73.5" r="52" fill="#1D1D1D" stroke="#414141"/>
-  <circle cx="344.5" cy="73.5" r="52" fill="#1D1D1D" stroke="#414141"/>
-  <text fill="#CACACA" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="30" letter-spacing="0em"><tspan x="63.4756" y="83.4004">Q2</tspan></text>
-  <path d="M223.5 73.5C223.5 102.214 199.999 125.5 171 125.5C142.001 125.5 118.5 102.214 118.5 73.5C118.5 44.7857 142.001 21.5 171 21.5C199.999 21.5 223.5 44.7857 223.5 73.5Z" fill="#1D1D1D" stroke="#414141"/>
-  <text fill="#CACACA" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="30" letter-spacing="0em"><tspan x="145.951" y="83.4004">135</tspan></text>
-  <text fill="#CACACA" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="30" letter-spacing="0em"><tspan x="245.005" y="83.4004">M</tspan></text>
-  <text fill="#CACACA" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="30" letter-spacing="0em"><tspan x="328.301" y="83.4004">18</tspan></text>
-  <text fill="#EDEDED" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="18" letter-spacing="0em"><tspan x="204.48" y="24.2402">53</tspan></text>
-  <text fill="#383838" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="18" letter-spacing="0em"><tspan id="UNIX" x="164.402" y="150.24">1684270622</tspan></text>
-  <script>
-    function increment() {
-      var text = document.getElementById("UNIX");
-      text.innerHTML = ++text.innerHTML;
+        return [t,y,q,d1,d2,h];
     }
-    setInterval(increment, 1000);
-  </script>
-  </svg>"
+
+    function getDayString(uint dayInt) pure public returns(string memory) {
+        string[7] memory daysOfWeek = ["S", "M", "T", "W", "Th", "F", "Sa"];
+        string memory dayString = daysOfWeek[dayInt];
+        return dayString;
+    }
+
+    function calendar() public view returns(string memory) {    
+        // get date time
+        uint[6] memory d = getDateTime();
+        string memory dayOfWeek = getDayString(d[4]);
+        // return SVG
+        return string.concat("<svg width=\"390\" height=\"160\" viewBox=\"0 0 390 150\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><circle cx=\"60\" cy=\"75\" r=\"52\" fill=\"#FEFEFE\" stroke=\"#D6D6D6\"/><circle cx=\"240\" cy=\"75\" r=\"52\" fill=\"#FEFEFE\" stroke=\"#D6D6D6\"/><circle cx=\"330\" cy=\"75\" r=\"52\" fill=\"#FEFEFE\" stroke=\"#D6D6D6\"/><circle cx=\"150\" cy=\"75\" r=\"52\" fill=\"#FEFEFE\" stroke=\"#D6D6D6\"/><text fill=\"#9E9E9E\" xml:space=\"preserve\" style=\"white-space: pre\" font-family=\"Arial\" font-size=\"28\" letter-spacing=\"0em\"><tspan x=\"15.3%\" y=\"52%\" dominant-baseline=\"middle\" text-anchor=\"middle\">Q",Strings.toString(d[2]),"</tspan></text><text fill=\"#9E9E9E\" xml:space=\"preserve\" style=\"white-space: pre\" font-family=\"Arial\" font-size=\"28\" letter-spacing=\"0em\"><tspan x=\"38.4%\" y=\"52%\" dominant-baseline=\"middle\" text-anchor=\"middle\">",Strings.toString(d[3]),"</tspan></text><text fill=\"#9E9E9E\" xml:space=\"preserve\" style=\"white-space: pre\" font-family=\"Arial\" font-size=\"28\" letter-spacing=\"0em\"><tspan x=\"61.6%\" y=\"52%\" dominant-baseline=\"middle\" text-anchor=\"middle\">",dayOfWeek,"</tspan></text><text fill=\"#9E9E9E\" xml:space=\"preserve\" style=\"white-space: pre\" font-family=\"Arial\" font-size=\"28\" letter-spacing=\"0em\"><tspan x=\"85%\" y=\"52%\" dominant-baseline=\"middle\" text-anchor=\"middle\">",Strings.toString(d[5]),"</tspan></text><text fill=\"#808080\" xml:space=\"preserve\" style=\"white-space: pre\" font-family=\"Arial\" font-size=\"18\" letter-spacing=\"0em\"><tspan x=\"50%\" y=\"10%\" dominant-baseline=\"middle\" text-anchor=\"middle\">",Strings.toString(d[1]),"</tspan></text><text fill=\"#E8E8E8\" xml:space=\"preserve\" style=\"white-space: pre\" font-family=\"Arial\" font-size=\"18\" letter-spacing=\"0em\"><tspan id=\"UNIX\" x=\"50%\" y=\"100%\" dominant-baseline=\"middle\" text-anchor=\"middle\">",Strings.toString(d[0]),"</tspan></text><script>function increment() {var text = document.getElementById(\"UNIX\");text.innerHTML = ++text.innerHTML;}setInterval(increment, 1000);</script></svg>");
+    }
+
+    function calendarDark() public view returns(string memory) {
+        // get date time
+        uint[6] memory d = getDateTime();
+        string memory dayOfWeek = getDayString(d[4]);
+        // return SVG
+        return string.concat("<svg width=\"390\" height=\"160\" viewBox=\"0 0 390 150\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><circle cx=\"60\" cy=\"75\" r=\"52\" fill=\"#1D1D1D\" stroke=\"#414141\"/><circle cx=\"240\" cy=\"75\" r=\"52\" fill=\"#1D1D1D\" stroke=\"#414141\"/><circle cx=\"330\" cy=\"75\" r=\"52\" fill=\"#1D1D1D\" stroke=\"#414141\"/><circle cx=\"150\" cy=\"75\" r=\"52\" fill=\"#1D1D1D\" stroke=\"#414141\"/><text fill=\"#CACACA\" xml:space=\"preserve\" style=\"white-space: pre\" font-family=\"Arial\" font-size=\"28\" letter-spacing=\"0em\"><tspan x=\"15.3%\" y=\"52%\" dominant-baseline=\"middle\" text-anchor=\"middle\">Q",Strings.toString(d[2]),"</tspan></text><text fill=\"#CACACA\" xml:space=\"preserve\" style=\"white-space: pre\" font-family=\"Arial\" font-size=\"28\" letter-spacing=\"0em\"><tspan x=\"38.4%\" y=\"52%\" dominant-baseline=\"middle\" text-anchor=\"middle\">",Strings.toString(d[3]),"</tspan></text><text fill=\"#CACACA\" xml:space=\"preserve\" style=\"white-space: pre\" font-family=\"Arial\" font-size=\"28\" letter-spacing=\"0em\"><tspan x=\"61.6%\" y=\"52%\" dominant-baseline=\"middle\" text-anchor=\"middle\">",dayOfWeek,"</tspan></text><text fill=\"#CACACA\" xml:space=\"preserve\" style=\"white-space: pre\" font-family=\"Arial\" font-size=\"28\" letter-spacing=\"0em\"><tspan x=\"85%\" y=\"52%\" dominant-baseline=\"middle\" text-anchor=\"middle\">",Strings.toString(d[5]),"</tspan></text><text fill=\"#EDEDED\" xml:space=\"preserve\" style=\"white-space: pre\" font-family=\"Arial\" font-size=\"18\" letter-spacing=\"0em\"><tspan x=\"50%\" y=\"10%\" dominant-baseline=\"middle\" text-anchor=\"middle\">",Strings.toString(d[1]),"</tspan></text><text fill=\"#383838\" xml:space=\"preserve\" style=\"white-space: pre\" font-family=\"Arial\" font-size=\"18\" letter-spacing=\"0em\"><tspan id=\"UNIX\" x=\"50%\" y=\"100%\" dominant-baseline=\"middle\" text-anchor=\"middle\">",Strings.toString(d[0]),"</tspan></text><script>function increment() {var text = document.getElementById(\"UNIX\");text.innerHTML = ++text.innerHTML;}setInterval(increment, 1000);</script></svg>");
+    }
+
+    function calendarSmall() public view returns(string memory) {
+        // get date time
+        uint[6] memory d = getDateTime();
+        string memory dayOfWeek = getDayString(d[4]);
+        return string.concat("");
+    }
+
+    function calendarSmallDark() public view returns(string memory) {
+        // get date time
+        uint[6] memory d = getDateTime();
+        string memory dayOfWeek = getDayString(d[4]);
+        return string.concat("");
+    }
+
+    function calendarGraphic() public view returns(string memory) {
+        // get date time
+        uint[6] memory d = getDateTime();
+        return string.concat("");
+    }
+
+    function calendarGraphicDark() public view returns(string memory) {
+        // get date time
+        uint[6] memory d = getDateTime();
+        return string.concat("");
+    }
 
 }
-
-
-
-
-
-// NOTES (DELETE AFTER FINAL SCRIPT DONE)
-
-// From chatGPT
-
-HOUR
-function getCurrentHour() public view pure returns (uint256) {
-        uint256 currentHour = (block.timestamp / 3600) % 24;
-        return currentHour;
-    }
-
-DAY (WEEK)
-function getCurrentDayOfWeek() public view pure returns (string memory) {
-        uint256 currentDayOfWeek = (block.timestamp / 86400 + 4) % 7;
-        string[7] memory daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        return daysOfWeek[currentDayOfWeek];
-    }
-
-DAY (QUARTER)
-function getCurrentQuarterAndDay() public view pure returns (uint256 quarter, uint256 day) {
-        uint256 secondsPerQuarter = 7884000; // 91.25 days in a quarter
-        uint256 elapsedSeconds = block.timestamp % secondsPerQuarter;
-        quarter = (uint256((block.timestamp / secondsPerQuarter) * 4) + 2) / 8 + 1;
-        day = elapsedSeconds / 86400 + 1;
-        return (quarter, day);
-    }
-
-
-QUARTER
-function getCurrentQuarter() public view pure returns (uint256) {
-        uint256 currentQuarter = (uint256((block.timestamp / 31536000) * 4) + 2) / 8 + 1;
-        return currentQuarter;
-    }
-(explanation)
-get the current UNIX timestamp and then divides it by the number
-of seconds in a non-leap year (31536000). This gives the number
-of years that have elapsed since the UNIX epoch. The number of
-years elapsed is then multiplied by 4, since there are four
-quarters in a year. The value 2 is added to this product to
-adjust the calculation for the midpoint of each quarter. The
-result is then divided by 8 and rounded up, which maps the
-calculated value to a value between 1 and 4, representing the
-current quarter of the year.
-
-YEAR
-function getCurrentYear public pure returns (uint256) {
-        uint256 currentYear = uint256(block.timestamp / 31536000) // number of seconds in a non-leap year
-        return currentYear;
-    }
